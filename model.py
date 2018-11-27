f41f3e1c80466b1babb82b9bb8c1ce6dd6383f6f
khalednakhleh <khaled.jkn@gmail.com>
2018-11-27 16:53:38 -0600

    modified model and added graphs
    Signed-off-by: khalednakhleh <khaled.jkn@gmail.com>

--- model.py
+++ model.py
@@ -8,21 +8,20 @@ Name: khalednakhleh
 
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Flatten, LSTM
 from keras import regularizers
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from keras.layers.convolutional import Conv1D, MaxPooling2D
+from keras.layers.convolutional import Conv1D, MaxPooling1D
 import numpy as np
 
 def main():
+    
     data = pd.read_csv("clean.csv")
-     
     data.astype("float32")
     
     # Normalizing the data per label
-    
     data.iloc[:, 0] /= max(data.iloc[:, 0])
     data.iloc[:, 1] /= max(data.iloc[:, 1])
     data.iloc[:, 2] /= max(data.iloc[:, 2])
@@ -34,20 +33,20 @@ def main():
     data.iloc[:, 8] /= max(data.iloc[:, 8])
     data.iloc[:, 9] /= max(data.iloc[:, 9])
 
-    df = data.iloc[:, 0:10]
-    y = data.iloc[:, 10]
+    X_train = (data.iloc[:, 0:10].values)
+    y_train = (data.iloc[:, 10].values)
     
-    #df = np.expand_dims(df, axis=2)
-    X_train, X_test, y_train, y_test = train_test_split(df, y, test_size = 0.2)
-
-
+    #X_train, X_test, y_train, y_test = train_test_split(df, y, test_size = 0.2)
+    X_train = np.expand_dims(X_train, axis=2)
+    
+    #print ("Validation data dimensions {} {}\n\n" .format(X_test.shape, y_test.shape))
+    
+    #X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
     print ("Training data dimensions {} {}" .format(X_train.shape, y_train.shape))
-    print ("Validation data dimensions {} {}\n\n" .format(X_test.shape, y_test.shape))
-    
 
     y_train = keras.utils.to_categorical(y_train)
-    y_test = keras.utils.to_categorical(y_test)
-    
+    #y_test = keras.utils.to_categorical(y_test)
+    """
     model = Sequential()
     model.add(Dense(512, input_dim = 10, activation='relu'))
     model.add(Dense(256, kernel_initializer='uniform',  kernel_regularizer=regularizers.l2(0.3), activation='relu'))
@@ -58,33 +57,36 @@ def main():
     model.add(Dense(6, kernel_initializer='uniform', activation='softmax'))
     
     """
+    
     model = Sequential()
-    model.add(Conv1D(32, 1, input_shape = (5,5,1), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv1D(64, 3, activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv1D(16, 3, input_shape = (10, 1), activation='relu'))
+    model.add(MaxPooling1D())
+    model.add(Dropout(0.4))
+    model.add(Conv1D(32, 1, activation='relu'))
+    model.add(MaxPooling1D())
+    model.add(Dropout(0.4))
+    model.add(Conv1D(64, 1, activation='relu'))
+    model.add(MaxPooling1D())
     model.add(Dropout(0.2))
     model.add(Flatten())
-    model.add(Dense(32, kernel_initializer='uniform',kernel_regularizer=regularizers.l2(0.01), activation='relu'))
-    model.add(Dense(16, kernel_initializer='uniform', kernel_regularizer=regularizers.l2(0.01), activation='relu'))
+    model.add(Dense(64, kernel_initializer='uniform', kernel_regularizer=regularizers.l2(0.1), activation='relu'))
+    model.add(Dense(32, kernel_initializer='uniform', kernel_regularizer=regularizers.l2(0.01), activation='relu'))
     model.add(Dense(6, kernel_initializer='uniform', activation='softmax'))
-    """
+    
     
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
+                  optimizer='sgd',
                   metrics=['accuracy'])
     
-    history = model.fit(X_train, y_train, epochs = 5,
-              batch_size = 1024,
-              validation_data = (X_test,y_test),
-              verbose = 2)
+    history = model.fit(X_train, y_train,
+              epochs = 5,  
+              batch_size= 1024)
     
-    loss, accuracy  = model.evaluate(X_test, y_test, verbose = False)
+    loss, accuracy  = model.evaluate(X_train, y_train)
 
     print (model.summary())
     plt.figure(1)
     plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
     plt.grid()
     plt.title('model accuracy')
     plt.ylabel('accuracy')
@@ -96,7 +98,6 @@ def main():
     plt.figure(2)
     
     plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
     plt.grid()
     plt.title('model loss')
     plt.ylabel('loss')
